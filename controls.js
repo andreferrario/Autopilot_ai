65f9415e7bf055e87c92776ce25216a43fbce740
andreferrario <51402874+andreferrario@users.noreply.github.com>
2022-05-23 20:16:04 +0200

    sensori del veicoli aggiunti

--- controls.js
+++ controls.js
@@ -1,51 +1,45 @@
 class Controls{
     constructor(){
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
         this.#addKeyboardListeners();
     }
 
-    // metodo privato 
     #addKeyboardListeners(){
-        document.onkeydown = (event) =>{
+        document.onkeydown=(event)=>{
             switch(event.key){
-                case 'ArrowLeft':
-                    this.left = true;
+                case "ArrowLeft":
+                    this.left=true;
                     break;
-                case 'ArrowRight':
-                    this.right = true;
+                case "ArrowRight":
+                    this.right=true;
                     break;
-                case 'ArrowUp':
-                    this.forward = true;
+                case "ArrowUp":
+                    this.forward=true;
                     break;
-                case 'ArrowDown':
-                    this.reverse = true;
+                case "ArrowDown":
+                    this.reverse=true;
                     break;
-                
             }
-
         }
-
-        document.onkeyup = (event) =>{
+        document.onkeyup=(event)=>{
             switch(event.key){
-                case 'ArrowLeft':
-                    this.left = false;
+                case "ArrowLeft":
+                    this.left=false;
                     break;
-                case 'ArrowRight':
-                    this.right = false;
+                case "ArrowRight":
+                    this.right=false;
                     break;
-                case 'ArrowUp':
-                    this.forward = false;
+                case "ArrowUp":
+                    this.forward=false;
                     break;
-                case 'ArrowDown':
-                    this.reverse = false;
-                    break;                
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
             }
-
-
         }
     }
 }
